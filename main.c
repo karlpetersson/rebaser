ca3a4ee7a58c796777bce16d4b86e25885c2357e
Karl Petersson <karl@limesaudio.com>
2016-12-22 11:51:05 +0100

    Validate input file, read lines and print them

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main(void) {
-    printf("TEst\n");
-    return 0;
+int main(int argc, char *argv[]) {
+    FILE *fp;
+        
+    if (argc < 2) {
+        fprintf(stderr, "Usage: reb <git-rebase-todo-file>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!(fp = fopen(argv[1], "rw"))) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    parse(fp);
+
+    exit(EXIT_SUCCESS);
+}
+
+int parse(FILE *fp) {
+    char line[256];
+
+    while(fgets(line, sizeof(line), fp)) {
+        printf("%s", line);
+    }
 }
